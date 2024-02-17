888bd86806acbe5a7cb86ba19cf6de61cc6b150b
codexswsh <swapnilshende29@gmail.com>
2024-02-17 07:57:08 +0530

    first commit

---
+++ src/components/notesContentDesktop/DesktopNotesContent.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import "./DesktopNotesContent.css";
+
+function DesktopNotesContent({ note }) {
+  return (
+    <div className="desktop__notes__content__note">
+      <div className="desktop__notes__content__details">
+        <p>{note.content}</p>
+      </div>
+      <div className="desktop__notes__content__date__time__details">
+        <div className="desktop__notes__content__date">{note.date}</div>
+        •
+        <div className="desktop__notes__content__time">{note.time}</div>
+      </div>
+    </div>
+  );
+}
+
+export default DesktopNotesContent;
---
+++ src/components/sidebarDesktop/DesktopSidebar.jsx
@@ -0,0 +1,69 @@
+import  { useEffect, useState } from "react";
+import "./DesktopSidebar.css";
+import CreateNotesPopup from "../createNotesPopupDesktop/CreateNotesPopup";
+import NotesTitle from "../notesSidebar/NotesTitle";
+
+function DesktopSidebar() {
+  const [titles, setTitles] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+  const [groupNamesParent, setGroupNamesParent] = useState(
+    localStorage.getItem("groupNames") || []
+  );
+
+  useEffect(() => {
+    const data = localStorage.getItem("groupNames");
+    if (data) {
+      setGroupNamesParent(JSON.parse(data));
+    } else {
+      setGroupNamesParent([]);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (groupNamesParent.length > 0) {
+      const obj = JSON.parse(localStorage.getItem("groupNames"));
+      const result = Object.keys(obj).map((key) => [obj[key]]);
+      setTitles(result);
+    }
+  }, [groupNamesParent]);
+
+  const handleClick = () => {
+    setShowPopup(true);
+  };
+
+  const handleClose = () => {
+    setShowPopup(false);
+  };
+
+  return (
+    <div className="desktop__sidebar">
+      <div className="desktop__sidebar__title">Pocket Notes</div>
+      
+      <div className="desktop__sidebar__notes__title">
+        {titles.length > 0 ? (
+          titles.map((title, index) => <NotesTitle key={index} title={title} />)
+        ) : (
+          <div className="desktop__sidebar__notes__title__empty">
+            <p>No Notes Group Created</p>
+          </div>
+        )}
+      </div>
+      <div className="desktop__sidebar__create__notes__btn">
+        <button onClick={()=>{setShowPopup(!showPopup)}}>
+          <span id="add">+</span>
+        </button>
+      </div>
+      {showPopup && (
+        <div className="desktop__popup__overlay">
+          <CreateNotesPopup
+            groupNamesParent={groupNamesParent}
+            setGroupNamesParent={setGroupNamesParent}
+            onClose={handleClose}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default DesktopSidebar;
